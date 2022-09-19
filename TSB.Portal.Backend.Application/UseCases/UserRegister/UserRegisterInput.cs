cd7dd360c38c8f7d2e0f4e3584e4d143be5ccfb4
antonio-cajueiro-campos <kimeesan5@gmail.com>
2022-09-19 14:32:56 -0300

    some fixes

--- TSB.Portal.Backend.Application/UseCases/UserRegister/UserRegisterInput.cs
+++ TSB.Portal.Backend.Application/UseCases/UserRegister/UserRegisterInput.cs
@@ -1,21 +1,22 @@
 using System.ComponentModel.DataAnnotations;
 using TSB.Portal.Backend.Application.EntitiesUseCase.DTO;
+using TSB.Portal.Backend.CrossCutting.Constants;
 using TSB.Portal.Backend.CrossCutting.Enums;
 using TSB.Portal.Backend.CrossCutting.Validation;
 
 namespace TSB.Portal.Backend.Application.UseCases.UserRegister;
 public class UserRegisterInput {
 	
-	[Required(ErrorMessage = "Preencha o campo Usuário.")]
+	[Required(ErrorMessage = Messages.RequiredUsername)]
 	public string Username { get; set; }
 	
-	[Required(ErrorMessage = "Preencha o campo Senha.")]
+	[Required(ErrorMessage = Messages.RequiredPassword)]
 	public string Password { get; set; }
 
-	[Required(ErrorMessage = "Preencha o campo Nome.")]
+	[Required(ErrorMessage = Messages.RequiredName)]
 	public string Name { get; set; }
 	
-	[Required(ErrorMessage = "Preencha o campo Documento.")]
+	[Required(ErrorMessage = Messages.RequiredDocument)]
 	[Document]
 	public string Document { get; set; }
 
--- TSB.Portal.Backend.CrossCutting/Constants/Messages.cs
+++ TSB.Portal.Backend.CrossCutting/Constants/Messages.cs
@@ -3,6 +3,7 @@ namespace TSB.Portal.Backend.CrossCutting.Constants
 {
     public static class Messages
     {
+		// Requests messages
 		public const string Created = "Criado com sucesso!";
 		public const string Deleted = "Apagado com sucesso!";
 		public const string Updated = "Atualizado com sucesso!";
@@ -17,5 +18,11 @@ namespace TSB.Portal.Backend.CrossCutting.Constants
 		public const string UserNotFound = "Usuário não encontrado.";
 		public static string UsernameAlreadyTaken(string username) => $"Usuário {username} já está em uso.";
 		public static string DocumentAlreadyTaken(string document) => $"Documento {document} já está em uso.";
+
+		// Fields messages
+		public const string RequiredUsername = "Preencha o campo Usuário.";
+		public const string RequiredPassword = "Preencha o campo Senha.";
+		public const string RequiredName = "Preencha o campo Nome.";
+		public const string RequiredDocument = "Preencha o campo Documento.";
     }
 }
