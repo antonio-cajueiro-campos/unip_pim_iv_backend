9ee708ba7070dfb1ea9d05d35e62717ba4159678
antonio-cajueiro-campos <kimeesan5@gmail.com>
2022-12-10 20:19:28 -0300

    fix

--- TSB.Portal.Backend.Infra/Repository/Extensions/DataExtensions.cs
+++ TSB.Portal.Backend.Infra/Repository/Extensions/DataExtensions.cs
@@ -19,7 +19,7 @@ public static class DataExtensions {
 	}
 
 	public static DbContextOptionsBuilder AddDefaultConnection(this DbContextOptionsBuilder  self) {
-		self.UseSqlServer("Server=bancodedadosunip.ddns.net;Initial Catalog=tsb;User Id=lucas;Password=123;");
+		self.UseSqlServer("Server=bancounip.ddns.net;Initial Catalog=tsb;User Id=sa;Password=0XamP45nCjHy;");
 		return self;
 	}
 }
